0efe86d2b95551226e5d79498d1fb01f9554063b
yakooza <yakooza@sysadmin>
2017-07-14 14:12:12 +0200

    Új osztályok

---
+++ java1git-teszt/src/java1git/teszt/Monitor.java
@@ -0,0 +1,12 @@
+package java1git.teszt;
+
+/**
+ *
+ * @author yakooza
+ */
+public class Monitor extends Eszkoz{
+    String marka;
+    String tipus;
+    String technika = "tft";
+    
+}
---
+++ java1git-teszt/src/java1git/teszt/Projektor.java
@@ -0,0 +1,13 @@
+
+package java1git.teszt;
+
+/**
+ *
+ * @author yakooza
+ */
+public class Projektor extends Eszkoz{
+    String leltariszam;
+    String marka;
+    String tipus; 
+    
+}
