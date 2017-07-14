ae8d9d2949748ee8aa4c46963bcd8a14207391f8
user <user@pb1>
2017-07-14 10:22:53 +0200

    Módosítottam a valamit. :)

--- java1git-teszt/src/java1git/teszt/Java1gitTeszt.java
+++ java1git-teszt/src/java1git/teszt/Java1gitTeszt.java
@@ -15,7 +15,7 @@ public class Java1gitTeszt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Gittest - ok.");
     }
     
 }
