72b00a7fdb003da5bb7b637d94dd2d1d80609954
yakooza <yakooza@sysadmin>
2017-07-14 07:51:58 +0200

    Számítógépek

---
+++ java1git-teszt/src/java1git/teszt/Szamitogep.java
@@ -0,0 +1,12 @@
+package java1git.teszt;
+
+/**
+ *
+ * @author yakooza
+ */
+public class Szamitogep extends Eszkoz{
+    String leltartiszama;
+    String memoria;
+    String processzor;
+    String videokartya;
+}
